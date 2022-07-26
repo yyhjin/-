d57dcc1ebf2c7e411d24d15eb86d4cf3e709f784
SSAFY <app928@naver.com>
2022-07-26 15:19:23 +0900

    setting:라우터 추가

---
+++ frontend/src/views/CustomerProfileView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>구매자 회원정보수정
+    </h1>
+  </div>
+</template>
---
+++ frontend/src/views/StoreProfileView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>가게정보수정</h1>
+  </div>
+</template>
