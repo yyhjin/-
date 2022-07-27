4e56c86428a9acf6af6b76d1baace58ef29a8c86
한재승 <gkswotmd96@naver.com>
2022-07-27 15:15:20 +0900

    구매자 회원가입 add

--- frontend/src/components/common/NavBar.vue
+++ frontend/src/components/common/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/join">회원가입</router-link> |
+      <router-link to="/selectjoin">회원가입</router-link> |
       <router-link to="/login">로그인</router-link>
   </nav>
 </template>
