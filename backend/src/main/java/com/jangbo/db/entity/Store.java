b31156b32566ba44e89cf80895b1735908b16e75
박종선 <jongsun1993@gmail.com>
2022-07-27 15:02:53 +0900

    :sparkles: Store entity add

---
+++ backend/src/main/java/com/jangbo/db/entity/Store.java
@@ -0,0 +1,54 @@
+package com.jangbo.db.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Store {
+
+    @Id
+    @GeneratedValue
+    private Long storeId;
+
+    @NotEmpty
+    private String storeName;
+    @NotEmpty
+    private String storeCategory;
+    @NotEmpty
+    private String storePhone;
+    @NotEmpty
+    private String storeAddr;
+
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "business_number")
+    private Seller seller;
+
+    private String marketNo;
+
+    private String storeIntro;
+    private String storeSubject;
+    private String storeImg;
+
+    //==생성 메서드==//
+    public static Store createStore(String storeName, String storeCategory, String storePhone, String storeAddr) {
+        Store store = new Store();
+        store.setStoreName(storeName);
+        store.setStoreCategory(storeCategory);
+        store.setStorePhone(storePhone);
+        store.setStoreAddr(storeAddr);
+
+        return store;
+    }
+
+
+}
