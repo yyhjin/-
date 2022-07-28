1e3fa0547d15c42679e43f35933c8fef448afaba
이경무 <app928@naver.com>
2022-07-28 20:43:07 +0900

    :sparkles: CustomerProfile

--- frontend/src/views/Customer/MyPageView.vue
+++ frontend/src/views/Customer/MyPageView.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    <span>{{msg}}#{{$route.params.id}}님</span>
+  <div class=root-div>
+    <h2>{{dummy.name}}#{{$route.params.id}}님의 page</h2>
     <el-card class="box-card">
       <template #header>
         <div class="card-header">
           <span>회원 정보</span>
+          <!-- 회원정보수정 버튼 -->
           <span class="button-chgprofile" @click="goProfile">회원 정보 수정</span>
         </div>
       </template>
-       ...
+       
+       <div class="userinfo-brief">
+        <!-- 간략한 회원정보  -->
+        {{dummy.name}}{{dummy.id}}
+       </div>
     </el-card>
 
     <el-card class="box-card">
@@ -18,6 +23,7 @@
         </div>
       </template>
       <div>
+        <!-- 주문 내역 component -->
         <OrderComp/>
       </div>
     </el-card>
@@ -27,16 +33,18 @@
 import OrderComp from '@/components/OrderComp.vue';
 export default {
     components: {OrderComp},
-    data(){
-        return {
-            msg: "한재승",
-        };
-    },
+    
+  data(){
+    return{
+      // dummy data from vuex
+      dummy : this.$store.state.userinfo
+    }
+  },
     methods: {
         goProfile() {
             this.$router.push({
                 name: "profile",
-                params: { id: this.$route.params.id }
+                // params: { user:this.dummy }
             });
         },
     },
@@ -44,6 +52,13 @@ export default {
 
 </script>
 <style scoped>
+.root-div{
+  /* width 주고,중앙 정렬. */
+  margin-left:auto; 
+  margin-right:auto;
+  width:390px;
+  
+}
 .card-header {
   display: flex;
   justify-content: space-between;
