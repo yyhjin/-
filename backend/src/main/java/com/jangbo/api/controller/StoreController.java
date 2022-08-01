ef98fe32ff24397da88aa71534718d47bc8e59b6
SSAFY <hseol96@naver.com>
2022-08-02 03:04:07 +0900

    :sparkles: Store Service,Controller add

--- backend/src/main/java/com/jangbo/api/controller/StoreController.java
+++ backend/src/main/java/com/jangbo/api/controller/StoreController.java
@@ -5,6 +5,8 @@ import com.jangbo.db.entity.Store;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -17,8 +19,16 @@ public class StoreController {
     StoreService storeService;
     @GetMapping("/{storeNo}")
     @ApiOperation(value = "상점api" , notes="설명",httpMethod = "GET")
-    public Optional<Store> getStore(@PathVariable int storeNo){
-        return storeService.selectBystoreNo(storeNo);
+    public ResponseEntity<Store> getStore(@PathVariable int storeNo){
+        Optional<Store> store = storeService.selectBystoreNo(storeNo);
+        return new ResponseEntity<Store>(store.get(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{storeNo}")
+    @ApiOperation(value = "상점api", notes="상점번호로 정보삭제",httpMethod = "DELETE")
+    public ResponseEntity<Void> deleteStore(@PathVariable int storeNo) {
+        storeService.deleteBystoreNo(storeNo);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 
 }
