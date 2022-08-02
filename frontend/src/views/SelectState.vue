75b0f20541e599a9f47026ef497ff1dca322be3a
이경무 <app928@naver.com>
2022-08-02 11:12:37 +0900

    :adhesive_bandage: bug fix

--- frontend/src/views/SelectState.vue
+++ frontend/src/views/SelectState.vue
@@ -47,11 +47,10 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .firstDiv {
     text-align: center;
 }
-
 .btn_group {  
     margin-top: 100px;
 }
@@ -73,7 +72,7 @@ export default {
 }
 
 img {
-    max-width: 100%;
+    max-width : 100%;
     max-height: 100%;
 }
 </style>
