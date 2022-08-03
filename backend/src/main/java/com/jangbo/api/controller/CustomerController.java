f81f02720ce23098c37ea4eafc5ce0eed2e7304c
박종선 <jongsun1993@gmail.com>
2022-08-03 18:01:30 +0900

    :sparkles: Customer 회원정보 조회 - 찜목록

--- backend/src/main/java/com/jangbo/api/controller/CustomerController.java
+++ backend/src/main/java/com/jangbo/api/controller/CustomerController.java
@@ -1,7 +1,7 @@
 package com.jangbo.api.controller;
 
 import com.jangbo.api.service.CustomerService;
-import com.jangbo.db.entity.Customer;
+import com.jangbo.db.entity.*;
 import com.jangbo.db.repository.CustomerRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,6 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.Objects.isNull;
 
@@ -70,20 +73,65 @@ public class CustomerController {
         return customerService.update(customerNo, request.getCustomerName(), request.getCustomerNickname(), request.getCustomerAddr(), request.getCustomerPhone());
     }
 
-////    회원정보 조회 - 찜목록
-//    @ApiOperation(value = "회원정보 조회 - 찜목록", notes = "소비자 찜목록을 조회한다.", httpMethod = "GET")
-//    @GetMapping("/customer/{customer_no}/interstore")
-//    public List<InterStoreDto> interStores() {
-//        List<InterStore> interStoreList =
+//    회원정보 조회 - 찜목록
+    @ApiOperation(value = "회원정보 조회 - 찜목록", notes = "소비자 찜목록을 조회한다.", httpMethod = "GET")
+    @GetMapping("/customer/{customer_no}/interstore")
+    public List<InterStoreDto> findInterStores(@PathVariable("customer_no") Integer customerNo) {
+        List<InterStore> interStores = customerRepository.findOne(customerNo).getInterStores();
+        List<InterStoreDto> result = interStores.stream()
+                .map(i -> new InterStoreDto(i))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+
+    //회원정보 조회 - 주문내역 조회
+//    @ApiOperation(value = "회원정보 조회 - 주문내역 조회", notes = "소비자 주문내역을 조회한다.", httpMethod = "GET")
+//    @GetMapping("/customer/{customer_no}/orders")
+//    public List<OrdersDto> findOrders(@PathVariable("customer_no") Integer customerNo) {
+//        List<Orders> orders = customerRepository.findOne(customerNo).getOrders();
+//        List<OrdersDto> result = orders.stream()
+//                .map(o -> new OrdersDto(o))
+//                .collect(Collectors.toList());
+//
+//        return result;
 //    }
 //
 //
 //    @Data
 //    @AllArgsConstructor
-//    static class InterStoreDto {
-//        private String
-//        private String
+//    static class OrdersDto<T> {
+//        private Integer orderNo;
+//        private String orderDate;
+//        private String storeNo;
+//        private String storeName; // store 에서 가져와야함
+//        private String status;
+//        private T orderItem; //
+//
+//        public OrdersDto(Orders order) {
+//            order.getOrderNo();
+//            order.getOrderDate();
+//            order.getStoreNo();
+//            order
+//            order.getStatus();
+//            order
+//        }
 //    }
+
+
+    @Data
+    @AllArgsConstructor
+    static class InterStoreDto {
+        private Integer storeNo;
+        private String storeName;
+
+        public InterStoreDto(InterStore interStore) {
+            Store store = interStore.getStore();
+            storeNo = store.getStoreNo();
+            storeName = store.getStoreName();
+        }
+
+    }
 
 
     @Data
