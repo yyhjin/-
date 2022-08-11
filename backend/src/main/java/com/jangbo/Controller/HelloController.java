6f1ba5c6823763714fe273304d5b8cf31dfb3503
yoonhyejin <wendy0301@naver.com>
2022-08-12 03:04:34 +0900

    openvidu-backend

--- backend/src/main/java/com/jangbo/Controller/HelloController.java
+++ backend/src/main/java/com/jangbo/Controller/HelloController.java
@@ -3,11 +3,11 @@ package com.jangbo.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HelloController {
-
-    @GetMapping
-    public String Hello(){
-        return "hello";
-    }
-}
+//@Controller
+//public class HelloController {
+//
+//    @GetMapping
+//    public String Hello(){
+//        return "hello";
+//    }
+//}
