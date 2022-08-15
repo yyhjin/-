2ab9b9f41b86eac2de435aec0fa2c0d6de5eb23f
yoonhyejin <wendy0301@naver.com>
2022-08-15 15:28:36 +0900

    :sparkles: CallOrder 삭제 시 orderNo 업데이트

--- backend/src/main/java/com/jangbo/db/dto/CallOrderDto.java
+++ backend/src/main/java/com/jangbo/db/dto/CallOrderDto.java
@@ -8,13 +8,11 @@ import lombok.RequiredArgsConstructor;
 @Data
 @RequiredArgsConstructor
 public class CallOrderDto {
-    private Integer callOrderNo ;
     private Integer orderNo;
     private String customerId;
     private Integer storeNo;
 
     public CallOrderDto(CallOrder callOrder) {
-        this.callOrderNo = callOrder.getCallOrderNo();
         this.orderNo = callOrder.getOrderNo();
         this.customerId = callOrder.getCustomerId();
         this.storeNo = callOrder.getStoreNo();
