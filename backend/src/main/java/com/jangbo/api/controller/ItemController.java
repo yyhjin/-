07df5c7e83e318374ac4b3c7eadafea53b43c623
SSAFY <hseol96@naver.com>
2022-08-15 13:47:27 +0900

    :wastebasket: delete Autowired

--- backend/src/main/java/com/jangbo/api/controller/ItemController.java
+++ backend/src/main/java/com/jangbo/api/controller/ItemController.java
@@ -10,6 +10,7 @@ import com.jangbo.db.entity.Item;
 import com.jangbo.db.entity.Store;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,17 +23,18 @@ import java.util.List;
 @Api(value="상품API",tags={"상품"})
 @RestController
 @RequestMapping("/be/item")
+@RequiredArgsConstructor
 public class ItemController {
 
-    @Autowired
-    ItemService itemService;
+
+    private final ItemService itemService;
 
     /*아이템 목록 조회*/
     @GetMapping("/store/{storeNo}")
     @ApiOperation(value = "한 상점안에 아이템 목록 api", notes="전체 조회(라이브상점에 뿌릴거임)",httpMethod = "GET")
     public ResponseEntity<List<Item>> findItems(@PathVariable("storeNo") Integer storeNo) {
         List<Item> itemList = itemService.findItemsByStore(storeNo);
-        return new ResponseEntity<List<Item>>(itemList, HttpStatus.OK);
+        return new ResponseEntity<>(itemList, HttpStatus.OK);
     }
 
     /* 아이템 등록 */
@@ -40,7 +42,7 @@ public class ItemController {
     @ApiOperation(value="상품 등록",notes = "상점에서 판매하는 상품을 등록한다.",httpMethod = "POST")
     public ResponseEntity<Integer> save(@RequestBody ItemRegisterPostReq itemRegisterPostReq){
         Integer savedItemNo = itemService.save(itemRegisterPostReq);
-        return new ResponseEntity<Integer>(savedItemNo,HttpStatus.CREATED);
+        return new ResponseEntity<>(savedItemNo,HttpStatus.CREATED);
     }
 
     /* 아이템 수정 */
@@ -48,7 +50,7 @@ public class ItemController {
     @ApiOperation(value = "상품 수정 api", notes="상품 정보 수정, 바뀐 부분만 업데이트 해준다.",httpMethod = "PATCH")
     public ResponseEntity<Integer> update(@PathVariable("itemNo") Integer itemNo,@RequestBody ItemEditPatchReq itemEditPatchReq) {
         Integer updatedItemNo = itemService.update(itemNo,itemEditPatchReq);
-        return new ResponseEntity<Integer>(updatedItemNo, HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(updatedItemNo, HttpStatus.NO_CONTENT);
     }
 
     /* 최근 사용여부 체크 */
@@ -56,7 +58,7 @@ public class ItemController {
     @ApiOperation(value = "최근사용여부 api", notes="상점 오픈할때 사용된 상품들은 최근 사용 true로 바꿔준다.",httpMethod = "PATCH")
     public ResponseEntity<Integer> recentUse(@PathVariable("itemNo") Integer itemNo, @RequestBody ItemRecentPatchReq itemRecentPatchReq) {
         Integer updatedItemNo = itemService.isRecent(itemNo,itemRecentPatchReq);
-        return new ResponseEntity<Integer>(updatedItemNo,HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(updatedItemNo,HttpStatus.NO_CONTENT);
     }
 
 
@@ -67,7 +69,7 @@ public class ItemController {
 
         itemService.delete(itemNo);
 
-        return new ResponseEntity<Integer>(itemNo, HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(itemNo, HttpStatus.NO_CONTENT);
     }
 
 
@@ -76,7 +78,7 @@ public class ItemController {
     @ApiOperation(value="상품 조회 api",notes = "상품번호로 상품정보를 조회한다.",httpMethod = "GET")
     public ResponseEntity<Item> selectByItemNo(@PathVariable("itemNo") Integer itemNo) {
         Item item = itemService.findItemByItemNo(itemNo);
-        return new ResponseEntity<Item>(item, HttpStatus.OK);
+        return new ResponseEntity<>(item, HttpStatus.OK);
     }
 
 
