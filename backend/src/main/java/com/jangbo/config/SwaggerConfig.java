ca8f7c7dedadb16673f517875e1f8b113c2dc87a
박종선 <jongsun1993@gmail.com>
2022-08-15 14:41:27 +0900

    :bulb: 주석제거 & 불필요한 파일 제거

--- backend/src/main/java/com/jangbo/config/SwaggerConfig.java
+++ backend/src/main/java/com/jangbo/config/SwaggerConfig.java
@@ -7,12 +7,9 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
-import java.util.List;
 
 //swagger address : http://localhost:8080/swagger-ui/#/
 @Configuration
