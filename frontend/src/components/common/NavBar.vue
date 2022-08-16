897bb6954cb1960bcdac10424419a1f4b7eb756e
yoonhyejin <wendy0301@naver.com>
2022-08-16 21:42:34 +0900

    :lipstick: Font, Logo, Navbar, MarketSearch

--- frontend/src/components/common/NavBar.vue
+++ frontend/src/components/common/NavBar.vue
@@ -1,60 +1,92 @@
 <template>
-    <nav class="nav_room" v-if="$route.path.substring(0, 5) == '/room'" style="display: flex; justify-content: space-between">
-        <span>room 안에 진입했을때 변경된 navbar</span>
-        <span>
-            <el-button
-                type="danger"
-                size="large"
-                @click="
-                    $router.push({
-                        name: 'mystore',
-                        params: { id: $store.getters['userInfo/isAuthenticated'] },
-                    })
-                "
-                >가게 닫기</el-button
-            >
-        </span>
-    </nav>
+  <nav
+    class="nav_room"
+    v-if="$route.path.substring(0, 5) == '/room'"
+    style="display: flex; justify-content: space-between"
+  >
+    <span>room 안에 진입했을때 변경된 navbar</span>
+    <span>
+      <el-button
+        type="danger"
+        size="large"
+        @click="
+          $router.push({
+            name: 'mystore',
+            params: { id: $store.getters['userInfo/isAuthenticated'] },
+          })
+        "
+        >가게 닫기</el-button
+      >
+    </span>
+  </nav>
 
-    <nav>
-        <img src="@/assets/jangbojang-logo.png" alt="logo" style="width: 100px" @click="cl_img()" />
-        <div class="div_nav" style="float: right; margin-top: 30px">
-            <router-link style="margin-right: 20px" to="/login" v-if="this.userNo == ''">로그인</router-link>
-            <router-link style="margin-right: 20px" to="/selectjoin" v-if="this.userNo == ''">회원가입</router-link>
+  <nav>
+    <img
+      src="@/assets/jangbojang-logo.png"
+      alt="logo"
+      style="width: 100px; margin: 6px"
+      @click="cl_img()"
+    />
+    <div class="div_nav" style="float: right; margin-top: 30px">
+      <router-link
+        style="margin-right: 20px"
+        to="/login"
+        v-if="this.userNo == ''"
+        >로그인</router-link
+      >
+      <router-link
+        style="margin-right: 20px"
+        to="/selectjoin"
+        v-if="this.userNo == ''"
+        >회원가입</router-link
+      >
 
-            <div v-if="this.userNo != ''" style="display: inline-block">
-                <el-dropdown>
-                    <el-button class="dropbtn"> MENU </el-button>
-                    <template #dropdown>
-                        <el-dropdown-menu>
-                            <el-dropdown-item v-if="this.userType == '구매자' && this.userNo != ''"><router-link to="/search" class="router-link">시장검색</router-link></el-dropdown-item>
-                            <el-dropdown-item v-if="this.userType == '판매자' && this.userNo != ''"
-                                ><router-link
-                                    class="router-link"
-                                    :to="{
-                                        name: 'mystore',
-                                        params: { id: $store.getters['userInfo/isAuthenticated'] },
-                                    }"
-                                    >내 가게</router-link
-                                ></el-dropdown-item
-                            >
-                            <el-dropdown-item v-if="this.userType == '구매자' && this.userNo != ''"
-                                ><router-link
-                                    class="router-link"
-                                    :to="{
-                                        name: 'mypage',
-                                        params: { id: $store.getters['userInfo/isAuthenticated'] },
-                                    }"
-                                    >마이페이지</router-link
-                                ></el-dropdown-item
-                            >
-                            <el-dropdown-item divided v-if="this.userNo != ''"><span style="margin-right: 10px; font-weight: bold" @click="logout">로그아웃</span></el-dropdown-item>
-                        </el-dropdown-menu>
-                    </template>
-                </el-dropdown>
-            </div>
-        </div>
-    </nav>
+      <div v-if="this.userNo != ''" style="display: inline-block">
+        <el-dropdown>
+          <el-button class="dropbtn"> MENU </el-button>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item
+                v-if="this.userType == '구매자' && this.userNo != ''"
+                ><router-link to="/search" class="router-link"
+                  >시장검색</router-link
+                ></el-dropdown-item
+              >
+              <el-dropdown-item
+                v-if="this.userType == '판매자' && this.userNo != ''"
+                ><router-link
+                  class="router-link"
+                  :to="{
+                    name: 'mystore',
+                    params: { id: $store.getters['userInfo/isAuthenticated'] },
+                  }"
+                  >내 가게</router-link
+                ></el-dropdown-item
+              >
+              <el-dropdown-item
+                v-if="this.userType == '구매자' && this.userNo != ''"
+                ><router-link
+                  class="router-link"
+                  :to="{
+                    name: 'mypage',
+                    params: { id: $store.getters['userInfo/isAuthenticated'] },
+                  }"
+                  >마이페이지</router-link
+                ></el-dropdown-item
+              >
+              <el-dropdown-item divided v-if="this.userNo != ''"
+                ><span
+                  style="margin-right: 10px; font-weight: bold"
+                  @click="logout"
+                  >로그아웃</span
+                ></el-dropdown-item
+              >
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -63,86 +95,90 @@ import { computed } from "vue";
 import { ElMessage } from "element-plus";
 
 export default {
-    name: "SearchAddress",
-    setup() {
-        const store = useStore();
-        const userType = computed(() => store.state.userInfo.userType);
-        const userNo = computed(() => store.state.userInfo.userNo);
+  name: "SearchAddress",
+  setup() {
+    const store = useStore();
+    const userType = computed(() => store.state.userInfo.userType);
+    const userNo = computed(() => store.state.userInfo.userNo);
 
-        const logOut = () => {
-            store.dispatch(`userInfo/logout`);
-        };
-        const logOutMarket = () => {
-            store.dispatch(`storeInMarket/logout`);
-        };
-        const out = () => {
-            ElMessage({
-                message: "로그아웃 완료",
-                type: "warning",
-            });
-        };
+    const logOut = () => {
+      store.dispatch(`userInfo/logout`);
+    };
+    const logOutMarket = () => {
+      store.dispatch(`storeInMarket/logout`);
+    };
+    const out = () => {
+      ElMessage({
+        message: "로그아웃 완료",
+        type: "warning",
+      });
+    };
 
-        return { userType, userNo, logOut, logOutMarket, out };
+    return { userType, userNo, logOut, logOutMarket, out };
+  },
+  methods: {
+    logout() {
+      console.log("logout");
+      this.logOut();
+      this.logOutMarket();
+      this.out();
+      this.$router.push({ name: "login" });
     },
-    methods: {
-        logout() {
-            console.log("logout");
-            this.logOut();
-            this.logOutMarket();
-            this.out();
-            this.$router.push({ name: "login" });
-        },
-        cl_img() {
-            return this.$router.push("/");
-        },
+    cl_img() {
+      return this.$router.push("/");
     },
+  },
 };
 </script>
 
 <style scoped>
+nav {
+  background-color: white;
+}
+
 .nav_room {
-    height: 40px;
+  height: 40px;
 }
 .logo {
-    max-width: 100%;
-    max-height: 100%;
+  max-width: 100%;
+  max-height: 100%;
 }
 
 nav a {
-    font-weight: bold;
-    color: #ff6f61;
-    text-decoration: none;
+  font-weight: bold;
+  color: #e07c49;
+  text-decoration: none;
 }
 
 nav a.router-link-exact-active {
-    color: #3cbd92;
+  color: #42413e;
 }
 
 .example-showcase .el-dropdown-link {
-    cursor: pointer;
-    color: #3cbd92;
-    display: flex;
-    align-items: center;
+  cursor: pointer;
+  color: #3cbd92;
+  display: flex;
+  align-items: center;
 }
 
 .router-link {
-    color: black;
-    text-decoration: none;
+  color: black;
+  text-decoration: none;
 }
 
 .dropbtn {
-    background-color: #ff6f61;
-    color: white;
-    padding: 16px;
-    font-size: 16px;
-    border: none;
-    cursor: pointer;
-    margin-top: -15px;
-    margin-right: 10px;
+  background-color: #e07c49;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+  margin-top: -15px;
+  margin-right: 10px;
 }
 
 .dropbtn:hover {
-    background-color: #3cbd92;
-    color: white;
+  background-color: #42413e;
+  color: white;
 }
 </style>
--- frontend/src/views/MarketListVIew.vue
+++ frontend/src/views/MarketListVIew.vue
@@ -1,25 +1,26 @@
 <template>
-    <div class="firstDiv">
-        <el-space wrap>
-            <el-card class="card1">
-                <div class="selectType" style="margin-top: 30px">
-                    <el-radio-group class="adjustC" v-model="searchType" size="large">
-                        <el-radio-button label="주소 검색" />
-                        <el-radio-button label="이름 검색" />
-                    </el-radio-group>
-                    <div v-if="searchType == '주소 검색'">
-                        <search-address></search-address>
-                    </div>
-                    <div v-else>
-                        <search-name></search-name>
-                    </div>
-                </div>
-            </el-card>
-        </el-space>
-        <div style="margin-top: 40px">
-            <search-list></search-list>
+  <div class="firstDiv">
+    <el-space wrap>
+      <div>시장을 검색하세요!</div>
+      <el-card class="card1" style="margin-top: 20px">
+        <div class="selectType" style="margin-top: 30px">
+          <el-radio-group class="adjustC" v-model="searchType" size="large">
+            <el-radio-button label="주소 검색" />
+            <el-radio-button label="이름 검색" />
+          </el-radio-group>
+          <div v-if="searchType == '주소 검색'">
+            <search-address></search-address>
+          </div>
+          <div v-else>
+            <search-name></search-name>
+          </div>
         </div>
+      </el-card>
+    </el-space>
+    <div style="margin-top: 40px">
+      <search-list></search-list>
     </div>
+  </div>
 </template>
 
 <script>
@@ -28,28 +29,28 @@ import SearchName from "@/components/SearchMarket/SearchName.vue";
 import SearchList from "@/components/SearchMarket/SearchList.vue";
 
 export default {
-    name: "MarketListView",
-    components: { SearchAddress, SearchName, SearchList },
-    data() {
-        return {
-            searchType: "주소검색",
-        };
-    },
+  name: "MarketListView",
+  components: { SearchAddress, SearchName, SearchList },
+  data() {
+    return {
+      searchType: "주소검색",
+    };
+  },
 };
 </script>
 
 <style scoped>
 .adjustC {
-    --el-color-primary: #ff6f61;
+  --el-color-primary: #7c7c7c;
 }
 
 .firstDiv {
-    text-align: center;
+  text-align: center;
 }
 
 .card1 {
-    width: 300px !important;
-    margin-top: 40px;
-    text-align: center;
+  width: 300px !important;
+  margin-top: 40px;
+  text-align: center;
 }
 </style>
