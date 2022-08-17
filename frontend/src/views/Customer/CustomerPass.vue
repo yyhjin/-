11ae52a41bc986ad294d02db098440c9e5e58b0d
yoonhyejin <wendy0301@naver.com>
2022-08-17 10:04:46 +0900

    :lipstick: Button, Color Change

--- frontend/src/views/Customer/CustomerPass.vue
+++ frontend/src/views/Customer/CustomerPass.vue
@@ -1,18 +1,32 @@
 <template>
-    <div>
-        <el-card class="box-card">
-            <div style="margin-top: -10px">
-                <h5 style="margin-left: 5px; margin-bottom: 10px">현재 비밀번호</h5>
-                <el-input v-model="olds" type="password" placeholder="password" show-password />
-                <h5 style="margin-left: 5px; margin-bottom: 10px">비밀번호 재입력</h5>
-                <el-input v-model="news" type="password" placeholder="password" show-password />
-            </div>
-            <div style="margin-top: 30px; text-align: right">
-                <el-button color="#e07c49" style="color: white" @click="cl_mo()">수정</el-button>
-                <el-button class="btn_cancle" @click="cl_cancle()">취소</el-button>
-            </div>
-        </el-card>
-    </div>
+  <div>
+    <el-card class="box-card">
+      <div style="margin-top: -10px">
+        <h5 style="margin-left: 5px; margin-bottom: 10px">현재 비밀번호</h5>
+        <el-input
+          v-model="olds"
+          type="password"
+          placeholder="password"
+          show-password
+        />
+        <h5 style="margin-left: 5px; margin-bottom: 10px">비밀번호 재입력</h5>
+        <el-input
+          v-model="news"
+          type="password"
+          placeholder="password"
+          show-password
+        />
+      </div>
+      <div style="margin-top: 30px; text-align: right">
+        <el-button round color="#42413e" style="color: white" @click="cl_mo()"
+          >수정</el-button
+        >
+        <el-button round class="btn_cancle" @click="cl_cancle()"
+          >취소</el-button
+        >
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -22,69 +36,69 @@ import { useStore } from "vuex";
 import { ElMessage } from "element-plus";
 
 export default {
-    name: "CustomerPass",
+  name: "CustomerPass",
 
-    setup() {
-        const store = useStore();
-        const userNo = computed(() => store.state.userInfo.userNo);
-        const olds = ref();
-        const news = ref();
+  setup() {
+    const store = useStore();
+    const userNo = computed(() => store.state.userInfo.userNo);
+    const olds = ref();
+    const news = ref();
 
-        const open1 = (message) => {
-            ElMessage({
-                showClose: true,
-                message: message,
-                type: "success",
-            });
-        };
-        const open2 = (message) => {
-            ElMessage({
-                showClose: true,
-                message: message,
-                type: "error",
-            });
-        };
+    const open1 = (message) => {
+      ElMessage({
+        showClose: true,
+        message: message,
+        type: "success",
+      });
+    };
+    const open2 = (message) => {
+      ElMessage({
+        showClose: true,
+        message: message,
+        type: "error",
+      });
+    };
 
-        return { userNo, olds, news, open1, open2 };
-    },
+    return { userNo, olds, news, open1, open2 };
+  },
 
-    methods: {
-        cl_mo() {
-            console.log(this.userNo);
-            setPass(
-                this.userNo,
-                { password: this.olds, passwordUpdate: this.news },
-                (response) => {
-                    if (response.data.response == "success") {
-                        this.open1("비밀번호 변경 성공!!");
-                    } else {
-                        this.open2("비밀번호 변경 실패..");
-                    }
-                },
-                (error) => {
-                    console.log(error);
-                }
-            );
+  methods: {
+    cl_mo() {
+      console.log(this.userNo);
+      setPass(
+        this.userNo,
+        { password: this.olds, passwordUpdate: this.news },
+        (response) => {
+          if (response.data.response == "success") {
+            this.open1("비밀번호 변경 성공!!");
+          } else {
+            this.open2("비밀번호 변경 실패..");
+          }
         },
-        cl_cancle() {
-            this.$router.go(-1);
-        },
+        (error) => {
+          console.log(error);
+        }
+      );
     },
+    cl_cancle() {
+      this.$router.go(-1);
+    },
+  },
 };
 </script>
 
 <style scoped>
 .card-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .text {
-    font-size: 14px;
+  font-size: 14px;
 }
 
 .box-card {
-    width: 300px;
+  width: 300px;
 }
 </style>
